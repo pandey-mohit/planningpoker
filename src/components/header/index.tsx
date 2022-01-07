66cfd03912559f63f1c2416953f15d09985e54c2
Mohit Pandey <pandey.mohit@live.com>
2022-01-07 23:24:36 +0530

    Filter header based on route

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,27 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { matchPath } from "react-router-dom"
 import { PrimaryButton } from "@fluentui/react"
 import styles from "./style.module.css"
 import { CreateRoom } from "../createroom"
 import { useBoolean } from '@fluentui/react-hooks'
 
-export const Header: React.FC = () => {
+const RightNav: React.FC = () => {
   const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true)
-  const { roomId } = useParams()
-  console.log(useParams())
+  return (
+    <>
+      <PrimaryButton onClick={toggleHideDialog}>Start new room</PrimaryButton>
+      <CreateRoom hideDialog={hideDialog} toggleHideDialog={toggleHideDialog} />
+    </>
+  )
+}
+
+export const Header: React.FC = () => {
+  const roomPage = matchPath("/room/:roomId", location.pathname)
   return (
     <header className={styles.header}>
       <span className={`ms-fontSize-28 ${styles.heading}`}>Scrum Pokermon</span>
       <span className={styles.filler}></span>
-      { roomId && (
-      <>
-        <PrimaryButton onClick={toggleHideDialog}>Start new room</PrimaryButton>
-        <CreateRoom hideDialog={hideDialog} toggleHideDialog={toggleHideDialog} />
-      </>
-      )}
+      { roomPage && <RightNav /> }
     </header>
   )
 }
